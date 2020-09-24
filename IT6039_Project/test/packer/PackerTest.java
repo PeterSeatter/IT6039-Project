0d8e20bee4743beec957170112537135b9d85cc0
PeterSeatter <56239159+PeterSeatter@users.noreply.github.com>
2020-09-24 22:15:32 +1200

    Add files via upload
    Test File updates which added a packer test, manifest test and box test

--- IT6039_Project/test/packer/PackerTest.java
+++ IT6039_Project/test/packer/PackerTest.java
@@ -5,19 +5,53 @@
  */
 package packer;
 
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.BeforeClass;
 
 /**
  *
  * @author Peter
  */
 public class PackerTest {
+    
+    public PackerTest() {
+    }
+    
     @BeforeClass
     public static void setUpClass() {
-        System.out.println("Testing Packer class...");
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of packProducts method, of class Packer.
+     */
+    @Test
+    public void testPackProducts() {
+        System.out.println("packProducts");
+        Customer c = null;
+        Depot d = null;
+        Manifest m = null;
+        List<Box> expResult = null;
+        List<Box> result = Packer.packProducts(c, d, m);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
     
 }
